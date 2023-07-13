f6a8f8a90bda499eda38edb11c158682b9c95bf5
Sheshmani Patel <128884158+SheshmaniPatel@users.noreply.github.com>
2023-07-14 00:28:12 +0530

    Update ji key.js

--- config/key.js
+++ config/key.js
@@ -1,5 +1,5 @@
 module.exports = {
   MongoURI:
     // " mongodb://127.0.0.1:27017/authantication",
-   "mongodb+srv://sheshmani47581:Q6DM2xbkSMVzqERw@cluster0.btnvkfh.mongodb.net/"
+   "mongodb://atlas-sql-64b04101c4e4ef3fe35d7031-kop1q.a.query.mongodb.net/sample_airbnb?ssl=true&authSource=admin"
 };

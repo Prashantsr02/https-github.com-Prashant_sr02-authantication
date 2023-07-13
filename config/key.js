7ef5302b19324d58f6cefad0782f7190dc95b39d
Sheshmani Patel <128884158+SheshmaniPatel@users.noreply.github.com>
2023-07-14 00:11:32 +0530

    Update key.js

--- config/key.js
+++ config/key.js
@@ -1,5 +1,5 @@
 module.exports = {
   MongoURI:
     // " mongodb://127.0.0.1:27017/authantication",
-   " mongodb://127.0.0.1:27017/backend"
+   "mongodb+srv://sheshmani47581:Q6DM2xbkSMVzqERw@cluster0.btnvkfh.mongodb.net/"
 };
